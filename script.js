33f55df4b0b657d2015f9366005c14db15cfc129
Git-Simon <simon.aich@hetic.net>
2021-03-20 14:31:26 +0100

    fixed id url

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 const urls = [
-  "NM2w_yRoehg", //1-effects
-  "xe226V-2wkI", //2-running
-  "jVicjMTIPk0", //3-swordplay
-  "BxWVIaHdkbo", //4-gunplay
-  "N1jSmRW2VlQ", //5-hand to hand fighting
-  "o9lT5MshbXI", //6-machine
-  "RIXClyhxL5A", //7-samurai champloo
-  "xYqe3kkliA", //8-cowboy bebop
-  "0DPcwGLI4iU", //9-food
-  "RAXMnzBmjso", //10-aircraft
-  "TneoJDExtN4", //11-machine 2 1
-  "2RGguJgWWd0", //12-magical transformation
-  "412dHswjHJs", //13-falling & flying
-  "60b_9aSaNvU", //14-action 1
-  "TPowKIC", //15-flcl
-  "jlOyEhJdyoo", //16-liquid
-  "8zqav3M35kM", //17-somebody that i used to know
-  "ORvEmKDjIHY", //18-aesthetic
-  "AaF7rXatU9E", //19-the girl next door 1
+  "NM2w_yRoehg", //0-effects
+  "xe226V-2wkI", //1-running
+  "jVicjMTIPk0", //2-swordplay
+  "BxWVIaHdkbo", //3-gunplay
+  "N1jSmRW2VlQ", //4-hand to hand fighting
+  "o9lT5MshbXI", //5-machine
+  "RIXClyhxL5A", //6-samurai champloo
+  "-xYqe3kkliA", //7-cowboy bebop
+  "0DPcwGLI4iU", //8-food
+  "RAXMnzBmjso", //9-aircraft
+  "TneoJDExtN4", //10-machine 2 1
+  "2RGguJgWWd0", //11-magical transformation
+  "412dHswjHJs", //12-falling & flying
+  "60b_9aSaNvU", //13-action 1
+  "TPowKIC-Q90", //14-flcl
+  "jlOyEhJdyoo", //15-liquid
+  "8zqav3M35kM", //16-somebody that i used to know
+  "ORvEmKDjIHY", //17-aesthetic
+  "AaF7rXatU9E", //18-the girl next door 1
 ];
 
 const pickUrl = (list) => {
